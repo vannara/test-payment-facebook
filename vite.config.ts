ada65a7f58793d94cadaa6d7641d8eed899e4432
Vannara <vannara.loch@gmail.com>
2025-10-02 16:04:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      preview: {
+        allowedHosts: true, // 👈 allow all hosts
+      },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
