1741c4aa603cd62c4e0c7f0bd7692761ec7557ec
vannara <vannara.loch@gmail.com>
2025-10-02 18:13:06 +0700

    use generate qr

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,49 @@ const app = express();
 app.use(cors()); // allow frontend localhost
 app.use(express.json());
 
+// app.post("/api/pay", async (req, res) => {
+//   try {
+//     const { amount, items } = req.body;
+//     const merchant_id = "ec461963";
+
+//     const tran_id = `txn_${Date.now()}`;
+//     const req_time = new Date().toISOString().slice(0, 19).replace("T", " ");
+//     // generate hash (PayWay spec)
+//     const hash = crypto
+//       .createHash("sha512")
+//       .update(merchant_id + tran_id + amount + req_time)
+//       .digest("hex");
+
+//     const payload = {
+//       merchant_id,
+//       req_time,
+//       tran_id,
+//       amount,
+//       items,
+//       payment_option: "abapay_khqr",
+//       hash,
+//       return_url: "https://test-payment-facebook.onrender.com/api/payment-result",
+//     };
+
+//     const apiUrl =
+//       "https://checkout-sandbox.payway.com.kh/api/payment-gateway/v1/payments/purchase";
+
+//     const paywayRes = await axios.post(apiUrl, payload, {
+//       headers: { "Content-Type": "application/json" },
+//     });
+
+//     res.json(paywayRes.data);
+//   } catch (err) {
+//     console.error("Payment API error:", err.response?.data || err.message);
+//     res.status(500).json({ error: "Payment failed" });
+//   }
+// });
 app.post("/api/pay", async (req, res) => {
   try {
-    const { amount, items } = req.body;
+    const amount = req.body.amount;
     const merchant_id = "ec461963";
-
-    const tran_id = `txn_${Date.now()}`;
+    const currency = "USD";
+    const tran_id = `txn_qr_${Date.now()}`;
     const req_time = new Date().toISOString().slice(0, 19).replace("T", " ");
     // generate hash (PayWay spec)
     const hash = crypto
@@ -29,13 +66,13 @@ app.post("/api/pay", async (req, res) => {
       req_time,
       tran_id,
       amount,
-      items,
+      currency,
       payment_option: "abapay_khqr",
-      hash,
+      hash
     };
 
     const apiUrl =
-      "https://checkout-sandbox.payway.com.kh/api/payment-gateway/v1/payments/purchase";
+      "https://checkout-sandbox.payway.com.kh//api/payment-gateway/v1/payments/generate-qr";
 
     const paywayRes = await axios.post(apiUrl, payload, {
       headers: { "Content-Type": "application/json" },
@@ -47,6 +84,8 @@ app.post("/api/pay", async (req, res) => {
     res.status(500).json({ error: "Payment failed" });
   }
 });
+
+// app.get("/api/payment-result, async (req, res) => {});")
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
 
